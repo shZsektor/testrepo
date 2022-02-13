204a2cd67a67d3a820f5241879fd5f1c1bc60269
Sharef Zubidat <sektor_sharef@hotmail.com>
2022-02-13 04:32:05 +0200

    inital commit

---
+++ lavagna/project/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+flag=0
+
+while [ $flag -eq 0 ]; do
+    result=$( curl lavanga_db:3306 2>/dev/null )
+    if [ $? -eq 1 ]; then
+        echo "Successfully curled it"
+        flag=1
+    else
+        echo "Failed to curl it!"
+    fi
+    sleep 2
+done
+
+java -Xms64m -Xmx128m -Ddatasource.dialect="${DB_DIALECT}" \
+-Ddatasource.url="${DB_URL}" \
+-Ddatasource.username="${DB_USER}" \
+-Ddatasource.password="${DB_PASS}" \
+-Dspring.profiles.active="${SPRING_PROFILE}" \
+-jar /app/lavagna-jetty-console.war --headless
+
